ae4a5a5518c9bcf74b8cd05c207394f11c4674f8
Juhye0k <kyoung1678@naver.com>
2024-12-13 19:44:38 +0900

    enh: 초기 모델

--- src/App.js
+++ src/App.js
@@ -1,39 +1,55 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react';
+import {useState,useEffect} from 'react'
+import axios from 'axios'
+import './App.css';
+import { Container } from './Container.js';
 
-import './App.css'
-
+const SERVER_URL = 'http://localhost:8080/api/songs'
 const App = () => {
-  const [message, setMessage] = useState('서버 접속 중...')
+  const [camera,setCamera]=useState([])
 
-  const fetchData = async () => {
-    try {
-      //const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:8080/api/message'
-      const apiUrl = process.env.REACT_APP_API_URL
-
-      const response = await fetch(apiUrl)
-
-      if (!response.ok) {
-        throw new Error('Network response was not ok')
-      }
-
-      const data = await response.json()
-
-      setMessage(data.status);
-    } catch (error) {
-      console.error('Error fetching data: ', error)
+  const getCamera = async()=>{
+    try{
+      const res=await axios.get(SERVER_URL)
+      console.log(res)
+      setCamera(res.data)
+    }catch(err){
+      console.log(err)
+      setCamera([])
     }
   }
 
-  useEffect(() => {
-    fetchData()
-  }, [])
+  useEffect(()=>{
+    getCamera()
+  },[])
 
   return (
     <div className="App">
-      <h1>Backend로부터 받은 메시지</h1>
-      <p>{message}</p>
+      <Header />
+      <CameraList title="다나와에서 검색해보고 싶은 카메라 목록" listCamera={camera} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>카메라 검색 웹사이트</h1>
+    </header>
+  );
+};
+
+const CameraList = (props) => {
+  return (
+    <div className="camera-list" style={{ textAlign: 'center' }}>
+      <h2 style={{ textAlign: 'center' }}>{props.title}</h2>
+      <div className="grid-layout">
+        {props.listCamera.map((camera) => (
+          <Container key={camera.id} camera={camera} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Container.js
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import { FaStar } from 'react-icons/fa';
+
+const Container = ({ camera }) => {
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
+  const togglePopup = () => {
+    setIsPopupVisible((prevState) => !prevState);
+  };
+  return (
+    <div className="container">
+      <img
+        src={`${camera.image}`}
+        alt={`이미지 ${camera.id}`}
+        className="camera-image"
+      />
+      <div className="camera-details">
+        <a
+          href={`https://search.danawa.com/dsearch.php?k1=${camera.model}`}
+          target="_blank"
+          rel="noreferrer"
+          className="camera-title"
+        >
+          {`${camera.model} (${camera.brand})`}
+        </a>
+        <div className="camera-rating" style={{ position: 'absolute', top: '15px', left: '15px' }}>
+          {[...Array(camera.rating)].map((_, index) => (
+            <FaStar key={index} />
+          ))}
+        </div>
+      </div>
+      <button className="description-button" onClick={togglePopup}>
+        {isPopupVisible ? '설명 닫기' : '설명 보기'}
+      </button>
+      {isPopupVisible && (
+        <div className="camera-popup">
+          <div className="popup-content">
+            <button className="close-button" onClick={togglePopup}>
+              &times;
+            </button>
+            <h2>{camera.model} 설명</h2>
+            <p>{camera.description}</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+export { Container };
